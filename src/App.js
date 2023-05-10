94e55f8e5a9c04be36faed480048dfa3bf859bac
Tarek Sallam <tarek@sallam.ca>
2023-05-10 17:10:00 -0400

    three implementation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 // APP COMPONENT (includes everything)
 import "./App.css";
 import Header from "./components/Header.js";
+import Main from "./components/three/Main.js";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
 
 export default App;
