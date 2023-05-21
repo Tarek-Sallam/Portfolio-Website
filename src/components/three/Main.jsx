c8e5c3ef0551020d6f2671d69a97dad2c4882687
Tarek Sallam <tarek@sallam.ca>
2023-05-21 14:18:29 -0400

    finished weird orb

--- src/components/three/Main.jsx
+++ src/components/three/Main.jsx
@@ -1,49 +1,62 @@
 import { Canvas, useFrame, useThree, extend } from "@react-three/fiber";
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, forwardRef, useMemo } from "react";
 import vertexShader from "./shaders/vertex.glsl.js";
+import vertexShader2 from "./shaders/vertex2.glsl.js";
 import fragmentShader from "./shaders/fragment.glsl.js";
 import { Environment, OrbitControls, Effects } from "@react-three/drei";
 import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
 
+import { Vector2 } from "three";
 extend({ UnrealBloomPass });
 
-/* function CustomShader() {
+function CustomShader() {
+  const uniforms = useMemo(
+    () => ({
+      u_time: {
+        value: 0.0,
+      },
+      u_mouse: {
+        value: new Vector2(0, 0),
+      },
+      u_viewport: {
+        value: new Vector2(0, 0),
+      },
+    }),
+    []
+  );
+
   return (
     <>
-      <meshStandardMaterial
-        onBeforeCompile={(shader) => {
-          console.log(shader);
-        }}
+      <shaderMaterial
+        uniforms={uniforms}
+        vertexShader={vertexShader}
+        fragmentShader={fragmentShader}
+        toneMapped={false}
       />
     </>
   );
-} */
-
-function OuterSphere(props) {
-  return (
-    <mesh>
-      <icosahedronGeometry args={props.args} />
-      <meshPhysicalMaterial
-        roughness={0}
-        metalness={0}
-        transmission={1}
-        clearcoat={1}
-        clearcoatRoughness={0.5}
-      />
-    </mesh>
-  );
 }
 
-function InnerSphere(props) {
+function WeirdSphere(props) {
+  const mesh = useRef();
+
+  useFrame((state) => {
+    const { clock, mouse, viewport } = state;
+    const uniforms = mesh.current.material.uniforms;
+    uniforms.u_time.value = clock.getElapsedTime();
+    uniforms.u_mouse.value.x += (mouse.x - uniforms.u_mouse.value.x) * 0.05;
+    uniforms.u_mouse.value.y += (mouse.y - uniforms.u_mouse.value.y) * 0.05;
+    mesh.current.material.uniforms.u_viewport.value = new Vector2(
+      viewport.width,
+      viewport.height
+    );
+  });
+
   return (
-    <mesh>
+    <mesh ref={mesh}>
       <icosahedronGeometry args={props.args} />
-      <meshStandardMaterial
-        opacity={0.5}
-        emissive="blue"
-        emissiveIntensity={2}
-        toneMapped={false}
-      />
+      {props.mat}
     </mesh>
   );
 }
@@ -51,17 +64,8 @@ function InnerSphere(props) {
 function Main() {
   return (
     <Canvas>
-      <Effects disableGamma>
-        <unrealBloomPass threshold={0.1} strength={1} radius={2} />
-      </Effects>
-      <OuterSphere args={[2, 50]} />
-      <InnerSphere args={[1, 50]} />
-      <directionalLight color="white" intensity={2} position={[10, 0, -14]} />
-      <directionalLight
-        color="white"
-        intensity={4}
-        position={[-10, -2, -12.5]}
-      />
+      <OrbitControls />
+      <WeirdSphere args={[1, 200]} mat={<CustomShader />} />
     </Canvas>
   );
 }
