3b6b4e207c13508554899ac0173c1219b87cea6f
Tarek Sallam <tarek@sallam.ca>
2023-05-25 16:42:41 -0400

    added useCurrentwdith hook and updated camera math

--- src/components/three/Scene.jsx
+++ src/components/three/Scene.jsx
@@ -1,26 +1,27 @@
 // import hooks from r3f
-import { useFrame, useThree } from "@react-three/fiber";
-import React, { useEffect } from "react";
+import { useFrame } from "@react-three/fiber";
+
+// import custom hook
+import useCurrentWidth from "../hooks/useCurrentWidth.jsx";
+
 // import componenets
 import WeirdSphere from "./WeirdSphere.jsx";
 import Lights from "./Lights.jsx";
 import OuterSphere from "./OuterSphere.jsx";
+import { Vector2 } from "three";
 
 // scene component
 function Scene() {
+  const dist = useCurrentWidth() > 576 ? 3.5 : 5;
   // frame hook to update camera using mouse x and y positions
   useFrame(({ camera, mouse }) => {
     camera.position.x += (mouse.x / 1.75 - camera.position.x) * 0.05;
     camera.position.y += (-mouse.y / 0.8 - camera.position.y) * 0.05;
+    let XY = new Vector2(camera.position.x, camera.position.y);
+    camera.position.z = dist * Math.sin(Math.acos(XY.length() / dist)); // update z position
     camera.lookAt(0, 0, 0); // update to look at center
   });
 
-  // set the initial camera position
-  const camera = useThree((state) => state.camera);
-  useEffect(() => {
-    camera.position.z = 3.5;
-    console.log("changed");
-  }, []);
   // JSX for all the scene components
   return (
     <>
