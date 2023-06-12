2d0a89dbcd764acb2e4545ca95484effc4d3b3a4
Tarek Sallam <tarek@sallam.ca>
2023-06-12 15:02:41 -0400

    more comments to scroll function

--- src/components/gsap/horizontalScroll.jsx
+++ src/components/gsap/horizontalScroll.jsx
@@ -1,8 +1,12 @@
+// animation library GSAP
 import gsap from "gsap";
-import Timer from "../functions/Timer.js";
-import { disableScroll, enableScroll } from "../functions/enableScroll.js";
 
+// custom pausable timer
+import Timer from "../functions/Timer.js";
+
+// FUNCTION FOR THE HORIZONTALLY SCROLLING TEXT
 function horizontalScroll({
+  // PROPS
   tlRef,
   viewport,
   container,
@@ -13,20 +17,25 @@ function horizontalScroll({
   target,
   timerRef,
 }) {
-  const children = container.children;
+  const children = container.children; // children of the passed container
   const width = children[0].offsetWidth; // width of text
-  const remaining = width - viewport; // text - width of viewport
-  const distance = width + viewport;
-  const overflow = width * (children.length / 2 - 1) - viewport;
-  const repeatWait = (overflow / distance) * duration;
-  const start = direction === 1 ? viewport : -width;
-  const spacingDuration = (spacing / distance) * duration;
-  tlRef.current = gsap.timeline();
-  const tl = tlRef.current;
+  const remaining = width - viewport; // text minus width of viewport
+  const distance = width + viewport; // distance needed to travel
+  const overflow = width * (children.length / 2 - 1) - viewport; // overflow distance of children and viewport
+  const repeatWait = (overflow / distance) * duration; // the delay time between repeating
+  const start = direction === 1 ? viewport : -width; // the start depending on direction
+  const spacingDuration = (spacing / distance) * duration; // duration that takes into account spacing
+  tlRef.current = gsap.timeline(); // create timeline
+  const tl = tlRef.current; // reference to timeline
+
+  // delay to change targets
   const changeTargetDuration =
     (viewport / 2 / distance + spacing * 1.25) * duration + spacingDuration;
 
+  // array for the starting positions
   let starts = [];
+
+  // change target function
   function changeTarget(newTarget, newTarget2) {
     if (target.current.length !== 0) {
       target.current[0].classList.remove("hero-target");
@@ -42,9 +51,11 @@ function horizontalScroll({
     target.current[1].classList.remove("hero-no-target");
   }
 
+  // if there isn't more than 1 child
   if (children.length < 4) {
     return;
   }
+
   // calculate the starts for all of the elements and set the starts
   for (let i = 0; i < children.length / 2; i++) {
     if (i === 0) {
@@ -62,9 +73,12 @@ function horizontalScroll({
         starts[i] = start;
       }
     }
+
+    // set position to the current starting position
     gsap.set(children[i * 2], { x: starts[i] });
     gsap.set(children[i * 2 + 1], { x: starts[i] });
 
+    // set the first target
     if (target.current.length === 0) {
       changeTarget(children[i * 2], children[i * 2 + 1]);
     } else if (
@@ -75,52 +89,65 @@ function horizontalScroll({
       changeTarget(children[i * 2], children[i * 2 + 1]);
     }
   }
+
   const end = direction === 1 ? -width : viewport; // find end point
 
-  for (let i = 0; i < children.length / 2; i++) {
-    if (starts[i] === start) {
-      break;
-    }
+  // initial for loop (for all the elements before the last one)
+  for (let i = 0; i < children.length / 2 - 1; i++) {
+    // calculate initial duration
+    const initDuration = (Math.abs(end - starts[i]) / distance) * duration;
 
-    let initDuration = (Math.abs(end - starts[i]) / distance) * duration;
-    let initChangeTargetDuration =
+    // calculate initial delay for changing target on element of loop instance
+    const initChangeTargetDuration =
       (Math.abs(viewport / 2 - (starts[i] + width / 2)) / distance) * duration;
 
-    tl.to(
+    // animation
+    const rootFirst = tl.to(
+      // select the current iteration items
       container.querySelectorAll("." + classN + (i + 1)),
       {
-        x: end,
-        ease: "none",
-        duration: initDuration,
+        x: end, // animate to end
+        ease: "none", // no ease for linear animation
+        duration: initDuration, // use initial duration
+
+        /* ON START: set a delay using the imported timer function 
+         (once delay is completed change the target to current instance) */
+        onStart: function () {
+          const timer = new Timer(function () {
+            delete timerRef[i];
+            if (rootFirst.isActive()) {
+              changeTarget(children[i * 2], children[i * 2 + 1]);
+            }
+          }, initChangeTargetDuration);
+          timerRef[i] = timer;
+        },
+
+        // ON COMPLETE: set the selected elements to the starting position
         onComplete: function () {
           gsap.set(container.querySelectorAll("." + classN + (i + 1)), {
             x: start,
           });
         },
-        onStart: function () {
-          const timer = new Timer(function () {
-            delete timerRef[i];
-            changeTarget(children[i * 2], children[i * 2 + 1]);
-          }, initChangeTargetDuration);
-          timerRef[i] = timer;
-        },
       },
-      "<"
+      "<" // start at beginning of previous animation
     );
   }
 
-  const root1 = tl.to(
+  // last element animation (starts offscreen)
+  const rootSecond = tl.to(
     container.querySelectorAll("." + classN + children.length / 2),
     {
       x: end,
       ease: "none",
       duration: duration,
-      repeat: -1,
+      repeat: -1, // repeat infinitely
+
+      // calculatethe proper repeat delay including the spacing
       repeatDelay: repeatWait + (children.length / 2) * spacingDuration,
       onStart: function () {
         const timer = new Timer(function () {
           delete timerRef[children.length / 2];
-          if (root1.isActive()) {
+          if (rootSecond.isActive()) {
             changeTarget(
               children[children.length - 2],
               children[children.length - 1]
@@ -129,10 +156,12 @@ function horizontalScroll({
         }, changeTargetDuration);
         timerRef[children.length / 2] = timer;
       },
+
+      // ON REPEAT: set the timer again for the repeat (same as above )
       onRepeat: function () {
         const timer = new Timer(function () {
           delete timerRef[children.length / 2 - 1];
-          if (root1.isActive()) {
+          if (rootSecond.isActive()) {
             changeTarget(
               children[children.length - 2],
               children[children.length - 1]
@@ -142,14 +171,11 @@ function horizontalScroll({
         timerRef[children.length / 2 - 1] = timer;
       },
     },
-    "<" + (repeatWait + (children.length / 2 - 1) * spacingDuration)
+    "<" + (repeatWait + (children.length / 2 - 1) * spacingDuration) // start after previous animation plus delay and spacing delay
   );
 
-  for (let j = 0; j < children.length / 2; j++) {
-    if (starts[j] === start) {
-      break;
-    }
-    const root2 = tl.to(
+  for (let j = 0; j < children.length / 2 - 1; j++) {
+    const rootThird = tl.to(
       container.querySelectorAll("." + classN + (j + 1)),
       {
         x: end,
@@ -160,7 +186,7 @@ function horizontalScroll({
         onStart: function () {
           const timer = new Timer(function () {
             delete timerRef[j];
-            if (root2.isActive()) {
+            if (rootThird.isActive()) {
               changeTarget(children[j * 2], children[j * 2 + 1]);
             }
           }, changeTargetDuration);
@@ -169,7 +195,7 @@ function horizontalScroll({
         onRepeat: function () {
           const timer = new Timer(function () {
             delete timerRef[j];
-            if (root2.isActive()) {
+            if (rootThird.isActive()) {
               changeTarget(children[j * 2], children[j * 2 + 1]);
             }
           }, changeTargetDuration);
