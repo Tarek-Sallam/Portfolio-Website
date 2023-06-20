22e898d1932e051fd763729cdf4ab90f6d9aeebe
Tarek Sallam <tarek@sallam.ca>
2023-06-20 14:33:55 -0400

    fixed css sizes

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -186,17 +186,17 @@ const HeroText = (props) => {
 
   return (
     <div className="hero-text-wrapper" ref={heroRef}>
-      <span className="hero-text-first" ref={firstNRef}>
+      <span className="hero-text-first hero-text-container" ref={firstNRef}>
         <p className="hero-text h-text-first difference h-text1">TAREK</p>
         <p className="hero-text h-text-first overlay h-text1">TAREK</p>
         <ListItems items={firstNames} />
       </span>
-      <span className="hero-text-last" ref={lastNRef}>
+      <span className="hero-text-last hero-text-container" ref={lastNRef}>
         <p className="hero-text h-text-last difference h-text1">SALLAM</p>
         <p className="hero-text h-text-last overlay h-text1">SALLAM</p>
         <ListItems items={lastNames} />
       </span>
-      <span className="hero-text-under" ref={underTextRef}>
+      <span className="hero-text-under hero-text-container" ref={underTextRef}>
         <p className="hero-text h-text-first difference">TAREK</p>
         <p className="hero-text h-text-first overlay">TAREK</p>
         <p className="hero-text h-text-last difference">SALLAM</p>
